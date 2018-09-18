9259416a11a196e55ec4f778da3738bbf042f1cc
AnkilP <a293pate@uwaterloo.ca>
2018-09-18 19:03:05 -0400

    Completed some helper functions

--- Double_sentinel_list.h
+++ Double_sentinel_list.h
@@ -1,9 +1,9 @@
 /*****************************************
- * UW User ID:  uwuserid
+ * UW User ID:  a293pate
  * Submitted for ECE 250
  * Department of Electrical and Computer Engineering
  * University of Waterloo
- * Calender Term of Submission:  (Winter|Spring|Fall) 20NN
+ * Calender Term of Submission:  Fall 2018
  *
  * By submitting this file, I affirm that
  * I am the author of all modifications to
@@ -23,10 +23,12 @@ class Double_sentinel_list {
 			public:
 				Double_node( Type const & = Type(), Double_node * = nullptr, Double_node * = nullptr );
 
+/////
 				Type value() const;
 				Double_node *previous() const;
 				Double_node *next() const;
 
+/////
 				Type         node_value;
 				Double_node *previous_node;
 				Double_node *next_node;
@@ -83,24 +85,53 @@ class Double_sentinel_list {
 //                      Public member functions                        //
 /////////////////////////////////////////////////////////////////////////
 
+/*
+ * The constructor creates two instances of a Double_node<Type> (called the sentinels). The head
+and tail pointers are set to point to one of the sentinels, each. The values stored in these nodes is
+not important, you can use the default value or whatever values you want. The previous and next
+pointers of the head sentinel should be nullptr and the address of the tail sentinel, respectively.
+The previous and next pointers of the tail sentinel should be the address of the head sentinel
+and nullptr, respectively. The node count is set to 0. (O(1))
+ */
+
 template <typename Type>
 Double_sentinel_list<Type>::Double_sentinel_list():
 // Updated the initialization list here
-list_head( nullptr ),
-list_tail( nullptr ),
-list_size( 0 )
+list_head(nullptr ),
+list_tail(nullptr ),
+list_size(0 )
 {
-	// Enter your implementation here
+    Double_node *sentinel_head = new Double_node(Type(), nullptr, nullptr);
+    Double_node *sentinel_tail = new Double_node(Type(), nullptr, nullptr);
+    list_head->next_node = sentinel_head;
+    list_tail->previous_node = sentinel_tail;
+    sentinel_head->next_node = sentinel_tail;
+    sentinel_tail->previous_node = sentinel_head;
 }
 
+/*
+ * The copy constructor must create a new doubly linked list with a copy of all of the nodes within the
+linked list pass as the argument list with the values stored in the same order. The linked list
+passed as an argument may not be changed. Once a copy is made, any change to the original linked
+list must not affect the copy. (O(n))
+ */
+
 template <typename Type>
-Double_sentinel_list<Type>::Double_sentinel_list( Double_sentinel_list<Type> const &list ):
+Double_sentinel_list<Type>::Double_sentinel_list(Double_sentinel_list<Type> const &list ):
 // Updated the initialization list here
-list_head( nullptr ),
-list_tail( nullptr ),
-list_size( 0 )
+list_head(nullptr ),
+list_tail(nullptr ),
+list_size(0 )
 {
-	// Enter your implementation here
+    Double_node *sentinel_head = new Double_node(Type(), nullptr, nullptr);
+    Double_node *sentinel_tail = new Double_node(Type(), nullptr, nullptr);
+    list_head->next_node = sentinel_head;
+    list_tail->previous_node = sentinel_tail;
+    for (int i = 0; i < list.size(); ++i) {
+        //TODO: go through each node in new list
+        list_size++;
+    }
+
 }
 
 template <typename Type>
@@ -120,56 +151,60 @@ Double_sentinel_list<Type>::~Double_sentinel_list() {
 
 template <typename Type>
 int Double_sentinel_list<Type>::size() const {
-	// Enter your implementation here
-	return 0;
+    return list_size;
 }
 
 template <typename Type>
 bool Double_sentinel_list<Type>::empty() const {
-	// Enter your implementation here
-	return true;
+    //TODO: maybe need to encapsulate in a try and catch
+    return list_head->next_node->next_node->next_node == list_tail ? true : false;
 }
 
 template <typename Type>
 Type Double_sentinel_list<Type>::front() const {
-	// Enter your implementation here
-	return Type(); // This returns a default value of Type
+    //TODO: encapsulte in try and catch
+    return list_head->next_node->next_node->node_value;
+    //     list head   sentinel    first node  node value
 }
 
 template <typename Type>
 Type Double_sentinel_list<Type>::back() const {
-	// Enter your implementation here
-	return Type(); // This returns a default value of Type
+    //TODO: encapsulate in try and catch
+    return list_tail->previous_node->previous_node->node_value;
+    //     list tail    sentinel tail    last node        node value
 }
 
 template <typename Type>
 typename Double_sentinel_list<Type>::Double_node *Double_sentinel_list<Type>::begin() const {
-	// Enter your implementation here
-	return nullptr;
+    return list_head->next_node.next();
+    //is this returning an address?
 }
 
 template <typename Type>
 typename Double_sentinel_list<Type>::Double_node *Double_sentinel_list<Type>::end() const {
-	// Enter your implementation here
-	return nullptr;
+    return list_tail->previous_node;
+    //     list tail  sentinel tail
 }
 
 template <typename Type>
 typename Double_sentinel_list<Type>::Double_node *Double_sentinel_list<Type>::rbegin() const {
-	// Enter your implementation here
-	return nullptr;
+    return list_tail->previous_node.previous();
+    //     list tail  sentinel tail last node
 }
 
 template <typename Type>
 typename Double_sentinel_list<Type>::Double_node *Double_sentinel_list<Type>::rend() const {
-	// Enter your implementation here
-	return nullptr;
+    return list_head->next_node;
 }
 
 template <typename Type>
 typename Double_sentinel_list<Type>::Double_node *Double_sentinel_list<Type>::find( Type const &obj ) const {
-	// Enter your implementation here
-	return nullptr;
+    for (auto *it = this->begin(); it != this->end(); it = it->next()) {
+        if (it->node_value == obj) {
+            return it;
+        }
+    }
+    return std::cout << "This value doesn't exist" << std::endl;
 }
 
 template <typename Type>
